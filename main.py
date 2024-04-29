8a3beae0f37e62866df45f6d1f6de135ce9bbdf4
YANGSEOKWOO <didtjrdn030@naver.com>
2024-04-29 21:11:16 +0900

    feat: 로그인 구현

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+import pyperclip
 import time
+
+global id
+global password
+
+def login():
+    id_input = driver.find_element(By.CSS_SELECTOR, "#id")
+    id_input.click()
+    pyperclip.copy(id)
+    # Window 운영체제라면 Keys.CONTROL
+    id_input.send_keys(Keys.COMMAND, 'v')
+
+    pw_input = driver.find_element(By.CSS_SELECTOR,'#pw')
+    pw_input.click()
+    pyperclip.copy(password)
+
+    pw_input.send_keys(Keys.COMMAND, 'v')
+
+    but = driver.find_element(By.CSS_SELECTOR, '#log\.login')
+    but.click()
 
 driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
 
@@ -10,8 +31,12 @@ url = "https://nid.naver.com/nidlogin.login"
 driver.get(url)
 
 print("아이디를 알려주세요")
+id = input()
+
 print("비밀번호를 입력하세요")
-time.sleep(5)
+password = input()
+
+login()
 
 # 현재 창 핸들 저장
 main_window_handle = driver.current_window_handle
@@ -28,3 +53,6 @@ while True:
     time.sleep(1)  # 1초마다 확인
 
 driver.quit()  # 드라이버 종료
+
+
+
